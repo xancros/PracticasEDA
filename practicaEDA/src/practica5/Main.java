c13b57343b96d2ad1771dd101d1f9f03f260525f
xancros <xancros69@gmail.com>
2013-12-04 11:41:24 +0100

    Hecho el ultimo ejercicio de diccionarios, aunque no se si es así pero es lo que se pide u.u
    Falta el ejercicio de tablas hash

--- practicaEDA/src/practica5/Main.java
+++ practicaEDA/src/practica5/Main.java
@@ -9,7 +9,7 @@ public class Main {
 	 */
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		AVLOrderedDict<Integer,String> dict = new AVLOrderedDict<Integer,String>();
+		/*AVLOrderedDict<Integer,String> dict = new AVLOrderedDict<Integer,String>();
 		dict.insert(1, "patata");
 		dict.insert(3, "comida");
 		dict.insert(8, "cielo");
@@ -18,7 +18,87 @@ public class Main {
 			System.out.print(entry.getKey()+" ");
 			System.out.println(entry.getValue());
 		}
-
+		*/
+		
+		BinarySearchTree<Integer> bst = new BinarySearchTree<Integer>();
+		AVLTree<Integer> avltree = new AVLTree<Integer>();
+		RBTree<Integer> rbtree = new RBTree<Integer>();
+		
+	}
+	public static void consultaOrdenada(BinarySearchTree<Integer> bst,AVLTree<Integer>avltree,RBTree<Integer>rbtree){
+		System.out.println("CONSULTA EN BINARY SEARCH TREE");
+		for(int i=1;i<=1000;i++){
+			bst.find(i);
+			System.out.println("consulta "+ i+ " en "+ +System.currentTimeMillis());
+			
+		}
+		System.out.println("CONSULTA EN AVL TREE");	
+		for(int i=1;i<=10000;i++){
+			avltree.find(i);
+			System.out.println("consulta "+ i+ " en "+ +System.currentTimeMillis());
+		}
+		System.out.println("CONSULTA EN RED BLACK TREE");
+		for(int i=1;i<=100000;i++){
+			rbtree.find(i);
+			System.out.println("consulta "+ i+ " en "+ +System.currentTimeMillis());
+		}
+		
+	}
+	public static void consultaAleatoria(BinarySearchTree<Integer> bst,AVLTree<Integer>avltree,RBTree<Integer>rbtree){
+		System.out.println("CONSULTA EN BINARY SEARCH TREE");
+		for(int i=1;i<=1000;i++){
+			bst.find((int)(Math.random()*1000)+1);
+			System.out.println("consulta "+ i+ " en "+ +System.currentTimeMillis());
+			
+		}
+		System.out.println("CONSULTA EN AVL TREE");	
+		for(int i=1;i<=10000;i++){
+			avltree.find((int)(Math.random()*1000)+1);
+			System.out.println("consulta "+ i+ " en "+ +System.currentTimeMillis());
+		}
+		System.out.println("CONSULTA EN RED BLACK TREE");
+		for(int i=1;i<=100000;i++){
+			rbtree.find((int)(Math.random()*1000)+1);
+			System.out.println("consulta "+ i+ " en "+ +System.currentTimeMillis());
+		}
+		
+	}
+	public static void insercionAleatoria(BinarySearchTree<Integer> bst,AVLTree<Integer>avltree,RBTree<Integer>rbtree){
+		System.out.println("INSERCION EN BINARY SEARCH TREE");
+		
+		for(int i=1;i<=1000;i++){
+			bst.insert((int)(Math.random()*1000)+1);
+			System.out.println("insercion "+ i+ " en "+ +System.currentTimeMillis());
+			
+		}
+		System.out.println("INSERCIONES EN AVL TREE");	
+		for(int i=1;i<=10000;i++){
+			avltree.insert((int)(Math.random()*10000)+1);
+			System.out.println("insercion "+ i+ " en "+ +System.currentTimeMillis());
+		}
+		System.out.println("INSERCIONES EN RED BLACK TREE");
+		for(int i=1;i<=100000;i++){
+			rbtree.insert((int)(Math.random()*100000)+1);
+			System.out.println("insercion "+ i+ " en "+ +System.currentTimeMillis());
+		}
+	}
+	public static void insercionOrdenada(BinarySearchTree<Integer> bst,AVLTree<Integer>avltree,RBTree<Integer>rbtree){
+		System.out.println("INSERCIONES EN BINARY SEARCH TREE");
+		for(int i=1;i<=1000;i++){
+			bst.insert(i);
+			System.out.println("insercion "+ i+ " en "+ +System.currentTimeMillis());
+			
+		}
+		System.out.println("INSERCIONES EN AVL TREE");	
+		for(int i=1;i<=10000;i++){
+			avltree.insert(i);
+			System.out.println("insercion "+ i+ " en "+ +System.currentTimeMillis());
+		}
+		System.out.println("INSERCIONES EN RED BLACK TREE");
+		for(int i=1;i<=100000;i++){
+			rbtree.insert(i);
+			System.out.println("insercion "+ i+ " en "+ +System.currentTimeMillis());
+		}
 	}
 
 }
